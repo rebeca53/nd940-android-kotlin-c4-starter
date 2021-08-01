adcf71c61513ed56ace5ec8e31ec3ad0b13ac9aa
rebeca <rebeca.n.rod@gmail.com>
2021-08-01 16:36:31 -0400

    Fix User cannot go to the selection location screen if the device location settings is off

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -59,13 +59,18 @@ class SaveReminderFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = this
         binding.selectLocation.setOnClickListener {
-            if (isLocationOn()) {
+            val foregroundLocationApproved = (
+                    PackageManager.PERMISSION_GRANTED ==
+                            ActivityCompat.checkSelfPermission(context!!,
+                                Manifest.permission.ACCESS_FINE_LOCATION)
+                    )
+            if (foregroundLocationApproved) {
                 //            Navigate to another fragment to get the user location
                 _viewModel.navigationCommand.value =
                     NavigationCommand.To(SaveReminderFragmentDirections.actionSaveReminderFragmentToSelectLocationFragment())
             } else {
                 Snackbar.make(binding.root,
-                    R.string.location_required_error,
+                    R.string.permission_denied_explanation,
                     Snackbar.LENGTH_LONG)
                     .show()
             }
